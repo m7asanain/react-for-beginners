5ae74b562234b9ee3fccab971f46eb3b743aa53a
Mustafa Alkamali <m7asanain@gmail.com>
2023-10-15 00:52:14 +0300

    conditional rendering

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,9 +1,19 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  //   if (items.length === 0) {
+  //     return <p>No Items found</p>;
+  //   }
+
+  //  this one is cleaner
+  const message = items.length === 0 ? <p>No Items found</p> : null;
 
   return (
     <>
       <h1>List</h1>
+      {/* {items.length === 0 ? <p>No Items found</p> : null} */}
+      {message}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">
