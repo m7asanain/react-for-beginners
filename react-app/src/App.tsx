e7d3be0f11b9de4a5c0a3b81b616682fe634d3be
Mustafa Alkamali <m7asanain@gmail.com>
2023-10-15 16:03:11 +0300

    execise: showing an alert / Mosh solution

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import BootstrapButton from "./components/BootstrapButton";
+
 // rafce
-
-import DismissingAlert from "./components/DismissingAlert";
-
 function App() {
+  const [alertVisibile, setAlertvisibility] = useState(false);
   return (
     <div>
-      <DismissingAlert />
+      {alertVisibile && (
+        <Alert onClose={() => setAlertvisibility(false)}>My Alert</Alert>
+      )}
+      <BootstrapButton color="primary" onClick={() => setAlertvisibility(true)}>
+        Button
+      </BootstrapButton>
     </div>
   );
 }
--- react-app/src/components/Alert.tsx
+++ react-app/src/components/Alert.tsx
@@ -4,10 +4,22 @@ interface Props {
   // if we want to pass the text as a child in App.tsx => change 'text' to  'children'
   // if we want to pass <span> or anything more than the string => change 'string' to 'ReactNode'
   children: ReactNode;
+  onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
+    </div>
+  );
 };
 
 export default Alert;
